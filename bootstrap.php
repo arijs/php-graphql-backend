031dd96320e9639adfa8ec1dd91a66fcdf0b6159
Rafael Hengles <rhengles@gmail.com>
2021-05-24 18:56:02 -0300

    usar graphiql with extensions

--- bootstrap.php
+++ bootstrap.php
@@ -8,5 +8,5 @@ use Siler\Monolog as Log;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-Config\load(__DIR__ . '/etc');
+Config\load(__DIR__ . '/etc', '/*.{ini,php}');
 Log\handler(new StreamHandler('php://stdout'));
